f9dad4b829bc20ee2063e8533882d8021be8ddd4
Tlhologelo2019 <85769877+Tlhologelo2019@users.noreply.github.com>
2024-05-12 13:43:26 +0200

    Dynamic lesson

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import EventListView from '../views/EventListView.vue'
+import EventDetailsView from '../views/EventDetailsView.vue'
 import AboutView from '../views/AboutView.vue'
-
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,11 +12,17 @@ const router = createRouter({
       component: EventListView,
     },
     {
+      path: '/event/:id',
+      name: 'event-details',
+      props: true,
+      component: EventDetailsView,
+    },
+    {
       path: '/about',
       name: 'about',
       component: AboutView,
-    }
-  ]
+    },
+  ],
 })
 
 export default router
